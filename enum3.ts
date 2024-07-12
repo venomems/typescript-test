638361b94eee2c79f6910afa20feae11082d494f
Александр <saneksrt222@gmail.com>
2024-07-12 04:42:42 +0400

    created function.ts

--- enum3.ts
+++ enum3.ts
@@ -1,3 +1,19 @@
 enum Membership {
-    
+    Simple,
+    Standard,
+    Premium
 }
+
+const membership = Membership.Standard;
+const membershipReverse = Membership[2];
+console.log(membership);
+console.log(membershipReverse);
+
+enum SocialMedia {
+    VK = 'vkontakte',
+    FACEBOOK = 'facebook',
+    INST = 'instagram'
+}
+
+const social = SocialMedia.INST
+console.log(social);
